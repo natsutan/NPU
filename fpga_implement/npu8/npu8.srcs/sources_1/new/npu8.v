46fe56b225b36a562aa7f9c9267a06a1fc63d9fd
natsutan <yrw03704@nifty.com>
2017-01-17 00:29:35 +0900

    add vivado project

---
+++ fpga_implement/npu8/npu8.srcs/sources_1/new/npu8.v
@@ -0,0 +1,13 @@
+module npu8 # (parameter QBIT = 8, FIXED_INT = 16, FIXED_FRAC = 16, ADR_WIDTH = 10)
+    (
+    input clk,
+    input reset_x,
+
+    //port A
+    ouput [ADR_WIDTH-1:0] RADR_A,
+    input [QBIT:0] RDATA_A.
+
+
+
+    );
+endmodule
