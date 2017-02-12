f2961889f2f807a859b6696934386b2081df9bcc
natsutan <yrw03704@nifty.com>
2017-02-12 17:48:40 +0900

    自動生成ツール追加

---
+++ tools/nnn_gen/nnn_gen.py
@@ -0,0 +1,10 @@
+import os
+
+# いろいろいったん決め打ちで
+
+input_model = '/home/natu/myproj/NPU/example/keres_cnn/keras/output/cnn.json'
+output_dir = '/home/natu/myproj/NPU/example/keres_cnn/c_ref/nnn_gen'
+
+output_cfile = 'nnn_gen.c'
+output_hfile = 'nnn_gen.h'
+
