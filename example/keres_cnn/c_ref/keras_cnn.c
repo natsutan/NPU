3a0bd6c9df8c56f094ac9ad8cbd7ebf5c970be4b
natsutan <yrw03704@nifty.com>
2017-02-12 09:48:00 +0900

    Cモデル作成中

--- example/keres_cnn/c_ref/keras_cnn.c
+++ example/keres_cnn/c_ref/keras_cnn.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "nnnet.h"
 
@@ -6,7 +7,30 @@
 
 int main(void)
 {
-  
+
+  //layrer
+  convolution2d_1.nb_filter = 32;
+  convolution2d_1.nb_row = 3;
+  convolution2d_1.nb_col = 3;
+  convolution2d_1.activation = LINEAR;
+  convolution2d_1.border_mode = BD_VALID;
+  convolution2d_1.input_dtype = NN_FLOAT32;
+  convolution2d_1.subsample[0] = 1;
+  convolution2d_1.subsample[1] = 1;
+  convolution2d_1.batch_input_shape[0] = 0;
+  convolution2d_1.batch_input_shape[1] = 28;
+  convolution2d_1.batch_input_shape[2] = 28;
+  convolution2d_1.batch_input_shape[3] = 1;
+
+  //NN
+  chiyaha_cnn.layernum = 1;
+
+  chiyaha_cnn.layer[0].type = TP_CONVOLUTION2D;
+  strcpy(chiyaha_cnn.layer[0].name, "convolution2d_1");
+  chiyaha_cnn.layer[0].p_param = &convolution2d_1;
+  chiyaha_cnn.layer[1].p_data = NULL;
+
+
   
 
   return 0;
--- example/keres_cnn/c_ref/keras_cnn.h
+++ example/keres_cnn/c_ref/keras_cnn.h
@@ -1,2 +1,14 @@
 
 
+//CNNの定義
+//将来的にはJSONから自動生成する。
+
+
+LY_Convolution2D convolution2d_1;
+
+
+
+
+
+//CNN定義
+NNNET chiyaha_cnn;
