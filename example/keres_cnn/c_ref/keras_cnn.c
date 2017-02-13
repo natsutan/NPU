9a923d1a629dfcba5ab3caa71ce1b7272b551cf9
natsutan <yrw03704@nifty.com>
2017-02-14 01:05:45 +0900

    変数宣言とＣの雛形が生成できるようになった。

--- example/keres_cnn/c_ref/keras_cnn.c
+++ example/keres_cnn/c_ref/keras_cnn.c
@@ -2,34 +2,15 @@
 #include <string.h>
 
 #include "nnnet.h"
+#include "nnn_gen/nnn_gen.h"
 
-#include "keras_cnn.h"
 
 int main(void)
 {
 
-  //layrer
-  convolution2d_1.nb_filter = 32;
-  convolution2d_1.nb_row = 3;
-  convolution2d_1.nb_col = 3;
-  convolution2d_1.activation = LINEAR;
-  convolution2d_1.border_mode = BD_VALID;
-  convolution2d_1.input_dtype = NN_FLOAT32;
-  convolution2d_1.subsample[0] = 1;
-  convolution2d_1.subsample[1] = 1;
-  convolution2d_1.batch_input_shape[0] = 0;
-  convolution2d_1.batch_input_shape[1] = 28;
-  convolution2d_1.batch_input_shape[2] = 28;
-  convolution2d_1.batch_input_shape[3] = 1;
 
-  //NN
-  chiyaha_cnn.layernum = 1;
-
-  chiyaha_cnn.layer[0].type = TP_CONVOLUTION2D;
-  strcpy(chiyaha_cnn.layer[0].name, "convolution2d_1");
-  chiyaha_cnn.layer[0].p_param = &convolution2d_1;
-  chiyaha_cnn.layer[1].p_data = NULL;
-  
+  NNNET *np;
+  np = nnn_init();
 
   return 0;
 }
